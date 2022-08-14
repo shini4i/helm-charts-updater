3b43c8aa2ce5248fee473350a1d4fdf0cdbea734
Vadims Gedzs <vadims@linux-tech.io>
2022-08-14 17:19:44 +0300

    feat: make helm.py use Chart model

--- helm_charts_updater/helm.py
+++ helm_charts_updater/helm.py
@@ -4,6 +4,7 @@ import semver
 import yaml
 
 from helm_charts_updater import config
+from helm_charts_updater.models import Chart
 
 
 class HelmChart:
@@ -12,18 +13,18 @@ class HelmChart:
         self.chart_name = config.get_chart_name()
         self.app_version = config.get_app_version()
 
-    def parse_charts_yaml(self) -> dict:
+    def parse_charts_yaml(self) -> Chart:
         print(f"===> Parsing {self.chart_name}'s Chart.yaml...")
         with open(f"charts/{self.charts_path}/{self.chart_name}/Chart.yaml", "r") as f:
             chart = f.read()
-        return yaml.safe_load(chart)
+        return Chart(**yaml.safe_load(chart))
 
     def update_chart_version(self) -> tuple:
         chart = self.parse_charts_yaml()
 
-        chart_version = semver.bump_patch(chart["version"])
+        chart_version = semver.bump_patch(chart.version)
         app_version = self.app_version
-        old_app_version = chart["appVersion"]
+        old_app_version = chart.appVersion
 
         if app_version == old_app_version:
             print(
@@ -31,14 +32,14 @@ class HelmChart:
             )
             exit(0)
 
-        print(f"===> Bumping chart version from {chart['version']} to {chart_version}")
-        chart["version"] = semver.bump_patch(chart["version"])
+        print(f"===> Bumping chart version from {chart.version} to {chart_version}")
+        chart.version = semver.bump_patch(chart.version)
 
-        print(f"===> Bumping app version from {chart['appVersion']} to {app_version}")
-        chart["appVersion"] = self.app_version
+        print(f"===> Bumping app version from {chart.appVersion} to {app_version}")
+        chart.appVersion = self.app_version
 
         with open(f"charts/{self.charts_path}/{self.chart_name}/Chart.yaml", "w") as f:
-            yaml.safe_dump(chart, sort_keys=False, stream=f)
+            yaml.safe_dump(chart.dict(exclude_none=True), sort_keys=False, stream=f)
 
         return chart_version, old_app_version
 
